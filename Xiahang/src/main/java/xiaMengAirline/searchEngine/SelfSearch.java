127e984df406929e74494b0fba9ffcc8d9652f14
lingouzhu <zhulingou@yaoo.com>
2017-07-12 20:44:06 +1000

    some slot

--- Xiahang/src/main/java/xiaMengAirline/searchEngine/SelfSearch.java
+++ Xiahang/src/main/java/xiaMengAirline/searchEngine/SelfSearch.java
@@ -1,12 +1,19 @@
 package xiaMengAirline.searchEngine;
 
+import xiaMengAirline.beans.Aircraft;
 import xiaMengAirline.beans.XiaMengAirlineSolution;
 
 public class SelfSearch {
 	public XiaMengAirlineSolution constructInitialSolution(XiaMengAirlineSolution originalSolution)
 			throws CloneNotSupportedException {
+		//when construct intial solution, clone a new copy
 		XiaMengAirlineSolution aNewSolution = originalSolution.clone();
 		
 		return aNewSolution;
+	}
+	
+	public Aircraft adjustAircraft (Aircraft originalAir) {
+		//when construct new plan for aircraft, no need clone?
+		return originalAir;
 	}
 }
