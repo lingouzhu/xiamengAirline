74fc5419b932fc82fc43df201e707b945aee6992
esunnen <sun.ningcen@ericsson.com>
2017-07-28 09:29:21 +0800

    cal cost by air

--- Xiahang/src/main/java/xiaMengAirline/util/Utils.java
+++ Xiahang/src/main/java/xiaMengAirline/util/Utils.java
@@ -14,6 +14,9 @@ import org.apache.commons.beanutils.BeanComparator;
 import org.apache.commons.collections4.ComparatorUtils;
 import org.apache.commons.collections4.comparators.ComparableComparator;
 
+import xiaMengAirline.beans.Aircraft;
+import xiaMengAirline.beans.Flight;
+
 public class Utils {
 
 
@@ -155,6 +158,63 @@ public class Utils {
 	    cl.add(Calendar.MINUTE, (int) minutes);
 	    return cl.getTime();
 	}
+	
+	public static BigDecimal calCostbyAir(Aircraft orgAir, Aircraft newAir) {
+		
+		BigDecimal cost = new BigDecimal("0");
+		// org air
+		for (int i = 0; i < orgAir.getFlightChain().size(); i++) {
+			
+			Flight orgFlight = orgAir.getFlightChain().get(i);
+			boolean existFlg = false;
+			// new air
+			for (int j = 0; j < newAir.getFlightChain().size(); j++) {
+				
+				Flight newFlight = newAir.getFlightChain().get(j);
+				// empty
+				if (i == 0 && newFlight.getFlightId() > InitData.plannedMaxFligthId) {
+					cost = cost.add(new BigDecimal("5000"));
+				}
+				// exist
+				if (orgFlight.getFlightId() == newFlight.getFlightId()) {
+					existFlg = true;
+					// delay or move up
+					if (!orgFlight.getDepartureTime().equals(newFlight.getDepartureTime())) {
+						BigDecimal hourDiff = Utils.hoursBetweenTime(newFlight.getDepartureTime(), orgFlight.getDepartureTime());
+						
+						if (hourDiff.signum() == -1){
+							cost = cost.add(new BigDecimal("150").multiply(hourDiff.abs()).multiply(orgFlight.getImpCoe()));
+						} else {
+							cost = cost.add(new BigDecimal("100").multiply(hourDiff.abs()).multiply(orgFlight.getImpCoe()));
+						}
+					}
+					// joint stretch
+					if (InitData.jointFlightMap.get(newFlight.getFlightId()) != null) {
+						if (!newFlight.getDesintationAirport().getId().equals((orgFlight.getDesintationAirport().getId()))) {
+							Flight nextFlight = InitData.jointFlightMap.get(newFlight.getFlightId());
+							
+							cost = cost.add(new BigDecimal("750").multiply(newFlight.getImpCoe()));
+							cost = cost.add(new BigDecimal("750").multiply(nextFlight.getImpCoe()));
+							
+						}
+						
+					}
+					
+				} 
+			}
+			
+			// cancel
+			if (!existFlg) {
+				// not 2nd of joint flight
+				if (!InitData.jointFlightMap.containsKey(orgFlight.getFlightId()) || InitData.jointFlightMap.get(orgFlight.getFlightId()) != null) {
+					cost = cost.add(new BigDecimal("1000").multiply(orgFlight.getImpCoe()));
+				}
+			}
+			
+		}
+		
+		return cost;
+    }
 	
 	
 	
