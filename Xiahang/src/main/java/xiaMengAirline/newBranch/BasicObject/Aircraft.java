9af6147a7fbc2b037ee89f771982e64d46392093
lingouzhu <zhulingou@yaoo.com>
2017-08-02 22:15:05 +1000

    change to right package

--- Xiahang/src/main/java/xiaMengAirline/newBranch/BasicObject/Aircraft.java
+++ Xiahang/src/main/java/xiaMengAirline/newBranch/BasicObject/Aircraft.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 
-import xiaMengAirline.beans.Flight;
+
 
 public class Aircraft {
 	private static final Logger logger = Logger.getLogger(Aircraft.class);
