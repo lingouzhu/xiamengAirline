a79bc3eaca90541af58fc9e6245ccc415558cf73
esunnen <sun.ningcen@ericsson.com>
2017-09-03 21:39:07 +0800

    new air flight id start with 9000

--- Xiahang/src/main/java/xiaMengAirline/beans/XiaMengAirlineSolution.java
+++ Xiahang/src/main/java/xiaMengAirline/beans/XiaMengAirlineSolution.java
@@ -90,7 +90,7 @@ public class XiaMengAirlineSolution implements Cloneable {
 			if (!aAir.isCancel()) {
 				for (Flight newFlight : aAir.getFlightChain()) {
 
-					if (newFlight.getFlightId() > InitData.maxFligthId) {
+					if (newFlight.getFlightId() > InitData.plannedMaxFligthId) {
 						cost = cost.add(new BigDecimal("5000"));
 						empty++;
 						if (refreshOut) {
