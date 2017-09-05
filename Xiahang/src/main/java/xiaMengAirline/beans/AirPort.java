48cd8bec996abf24e4f473e144d54c32da4c4892
lingouzhu <zhulingou@yaoo.com>
2017-09-05 15:11:02 +1000

    uml example

--- Xiahang/src/main/java/xiaMengAirline/beans/AirPort.java
+++ Xiahang/src/main/java/xiaMengAirline/beans/AirPort.java
@@ -14,6 +14,9 @@ import xiaMengAirline.Exception.AirportNotAvailable;
 import xiaMengAirline.Exception.SolutionNotValid;
 /**
 * @startuml
+* component basicobject
+* @enduml
+* @startuml
 * autonumber
 * SelfSearch -> AirPort: requestAirport
 * @enduml
